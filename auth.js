30aea6fdbce5ab7e4c437d26d6da21ed99baa178
reagan505 <reagangachanja6@gmail.com>
2024-07-20 14:12:56 +0300

    modified the page

---
+++ auth.js
@@ -0,0 +1,133 @@
+
+// require('dotenv').config();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBGERXSm-2BnTr6LD4WKfVbAk3GaqUoUUE",
+  authDomain: "aqua-africa-68dfe.firebaseapp.com",
+  projectId: "aqua-africa-68dfe",
+  storageBucket: "aqua-africa-68dfe.appspot.com",
+  messagingSenderId: "253039574589",
+  appId: "1:253039574589:web:75bcdcfbb81b9481ed7454",
+  measurementId: "G-041RXFX7KW"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+const main = document.getElementById("main");
+const returnBtn = document.getElementById("return-btn");
+
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("submit");
+
+const signupEmailIn = document.getElementById("email-signup");
+const confirmSignupEmailIn = document.getElementById("confirm-email-signup");
+const signupPasswordIn = document.getElementById("password-signup");
+const confirmSignUpPasswordIn = document.getElementById(
+  "confirm-password-signup"
+);
+const createacct = document.getElementById("create-acct");
+
+// Start with this
+const createacctbtn = document.getElementById("create-acct-btn");
+const signupButton = document.getElementById("sign-up");
+
+var email,
+  password,
+  signupEmail,
+  signupPassword,
+  confirmSignupEmail,
+  confirmSignUpPassword;
+
+createacctbtn.addEventListener("click", function () {
+  var isVerified = true;
+
+  signupEmail = signupEmailIn.value;
+  confirmSignupEmail = confirmSignupEmailIn.value;
+  if (signupEmail != confirmSignupEmail) {
+    window.alert("Email fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  signupPassword = signupPasswordIn.value;
+  confirmSignUpPassword = confirmSignUpPasswordIn.value;
+  if (signupPassword != confirmSignUpPassword) {
+    window.alert("Password fields do not match. Try again.");
+    isVerified = false;
+  }
+
+  if (
+    signupEmail == null ||
+    confirmSignupEmail == null ||
+    signupPassword == null ||
+    confirmSignUpPassword == null
+  ) {
+    window.alert("Please fill out all required fields.");
+    isVerified = false;
+  }
+
+  if (isVerified) {
+    createUserWithEmailAndPassword(auth, signupEmail, signupPassword)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        window.alert("Success! Account created.");
+        window.location = "./index.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        window.alert("Error occurred. Try again.");
+        window.alert(errorMessage);
+      });
+  }
+});
+
+submitButton.addEventListener("click", function () {
+  email = emailInput.value;
+  console.log(email);
+  password = passwordInput.value;
+  console.log(password);
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      window.alert("Success! Welcome back!");
+      window.location = "./index.html";
+
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      window.alert("Error occurred. Try again.");
+    });
+});
+
+// Navigate through the login / Register components
+
+signupButton.addEventListener("click", () => {
+  main.style.display = "none";
+  createacct.style.display = "block";
+});
+
+returnBtn.addEventListener("click", function () {
+  main.style.display = "block";
+  createacct.style.display = "none";
+});
